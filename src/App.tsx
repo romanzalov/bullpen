9bc4e8b841b86715772374f78f84cd5f6a847484
Roman Zalov <romanzalov@gmail.com>
2024-08-26 02:25:19 -0500

    price and time adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,19 @@ import { ArrowUpOutlined, ArrowDownOutlined } from '@ant-design/icons';
 import ReactECharts from 'echarts-for-react';
 import type { EChartsOption } from 'echarts';
 
+const BitcoinLogo: React.FC = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" style={{ verticalAlign: 'middle', marginRight: '8px' }}>
+    <g fill="none" fillRule="evenodd">
+      <circle cx="16" cy="16" r="16" fill="#F7931A"/>
+      <path fill="#FFF" fillRule="nonzero" d="M23.189 14.02c.314-2.096-1.283-3.223-3.465-3.975l.708-2.84-1.728-.43-.69 2.765c-.454-.114-.92-.22-1.385-.326l.695-2.783L15.596 6l-.708 2.839c-.376-.086-.746-.17-1.104-.26l.002-.009-2.384-.595-.46 1.846s1.283.294 1.256.312c.7.175.826.638.805 1.006l-.806 3.235c.048.012.11.03.18.057l-.183-.045-1.13 4.532c-.086.212-.303.531-.793.41.018.025-1.256-.313-1.256-.313l-.858 1.978 2.25.561c.418.105.828.215 1.231.318l-.715 2.872 1.727.43.708-2.84c.472.127.93.245 1.378.357l-.706 2.828 1.728.43.715-2.866c2.948.558 5.164.333 6.097-2.333.752-2.146-.037-3.385-1.588-4.192 1.13-.26 1.98-1.003 2.207-2.538zm-3.95 5.538c-.533 2.147-4.148.986-5.32.695l.95-3.805c1.172.293 4.929.872 4.37 3.11zm.535-5.569c-.487 1.953-3.495.96-4.47.717l.86-3.45c.975.243 4.118.696 3.61 2.733z"/>
+    </g>
+  </svg>
+);
+
 const { Title, Text } = Typography;
+
+
+
 
 interface StockDataPoint {
   timestamp: number;
@@ -57,14 +69,7 @@ const preGeneratedData: Record<Timeframe, StockDataPoint[]> = {
   '365': generateStaticData('365', 60000),
 };
 
-const BitcoinLogo: React.FC = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" style={{ verticalAlign: 'middle', marginRight: '8px' }}>
-    <g fill="none" fillRule="evenodd">
-      <circle cx="16" cy="16" r="16" fill="#F7931A"/>
-      <path fill="#FFF" fillRule="nonzero" d="M23.189 14.02c.314-2.096-1.283-3.223-3.465-3.975l.708-2.84-1.728-.43-.69 2.765c-.454-.114-.92-.22-1.385-.326l.695-2.783L15.596 6l-.708 2.839c-.376-.086-.746-.17-1.104-.26l.002-.009-2.384-.595-.46 1.846s1.283.294 1.256.312c.7.175.826.638.805 1.006l-.806 3.235c.048.012.11.03.18.057l-.183-.045-1.13 4.532c-.086.212-.303.531-.793.41.018.025-1.256-.313-1.256-.313l-.858 1.978 2.25.561c.418.105.828.215 1.231.318l-.715 2.872 1.727.43.708-2.84c.472.127.93.245 1.378.357l-.706 2.828 1.728.43.715-2.866c2.948.558 5.164.333 6.097-2.333.752-2.146-.037-3.385-1.588-4.192 1.13-.26 1.98-1.003 2.207-2.538zm-3.95 5.538c-.533 2.147-4.148.986-5.32.695l.95-3.805c1.172.293 4.929.872 4.37 3.11zm.535-5.569c-.487 1.953-3.495.96-4.47.717l.86-3.45c.975.243 4.118.696 3.61 2.733z"/>
-    </g>
-  </svg>
-);
+
 
 const BitcoinPriceChart: React.FC = () => {
   const [timeframe, setTimeframe] = useState<Timeframe>('1d');
@@ -151,27 +156,20 @@ const BitcoinPriceChart: React.FC = () => {
     dataZoom: [
       {
         type: 'inside',
-        start: 0,
-        end: 100
+        xAxisIndex: [0],
+        filterMode: 'filter'
       },
       {
-        start: 0,
-        end: 100,
-        handleSize: '80%',
-        handleStyle: {
-          color: '#fff',
-          shadowBlur: 3,
-          shadowColor: 'rgba(0, 0, 0, 0.6)',
-          shadowOffsetX: 2,
-          shadowOffsetY: 2
-        }
+        type: 'inside',
+        yAxisIndex: [0],
+        filterMode: 'empty'
       }
     ],
     grid: {
-      left: '3%',
-      right: '3%',
-      bottom: '10%',
-      top: '10%',
+      left: 0,
+      right: 0,
+      bottom: 0,
+      top: 0,
       containLabel: false
     }
   };
@@ -205,15 +203,14 @@ const BitcoinPriceChart: React.FC = () => {
 
   return (
     <div style={{ 
-      width: '100%', 
-      maxWidth: '100vw', 
-      overflow: 'hidden', 
-      padding: '10px',
-      boxSizing: 'border-box'
+      width: '100vw', 
+      maxWidth: '100%', 
+      overflow: 'hidden',
+      margin: 0,
+      padding: 0
     }}>
       <div style={{ 
         width: '100%', 
-        maxWidth: '600px', 
         margin: '0 auto'
       }}>
         <div style={{ textAlign: 'center', marginBottom: '20px' }}>
@@ -233,11 +230,11 @@ const BitcoinPriceChart: React.FC = () => {
             </Text>
           )}
         </div>
-        <div style={{ width: '100%', overflow: 'hidden' }}>
+        <div style={{ width: '100vw', overflow: 'hidden' }}>
           <ReactECharts
             ref={chartRef}
             option={options}
-            style={{ height: '300px', width: '100%' }}
+            style={{ height: '300px', width: '100vw' }}
             opts={{ renderer: 'svg' }}
           />
         </div>
